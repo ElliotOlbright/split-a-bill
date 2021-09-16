f663252b0af87630a58654cec9578aa74e489f60
Robert Gu <robertgu84@gmail.com>
2021-09-16 11:10:17 -0600

    CC roulette start

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
 # Intro to JavaScript
 
 ## Why JavaScript?
+* We use JS to query and manipulate the DOM(html)
 * Reloading the page for functionality is not fun
+* It is responsible for any event handling
 
 ## Architecture / Rails Ecosystem
+* JavaScript files get included via a manifest called application.js
 
 ## Event Handling
+* When we click on a web app, scroll, type something into an input box
+something happens as a result
 
 ## Fetch
 
--- app/assets/javascripts/bill_splitter.js
+++ app/assets/javascripts/bill_splitter.js
@@ -1,20 +1,33 @@
-  // Feature 1
-  // As a user
-  // When I enter in a postive dollar amount into total bill
-  // And I click on SPLIT THE BILL
-  // Then I expect to see that amount divvied up amongst the 4 patrons equally
+// Feature 1
+// As a user
+// When I enter in a postive dollar amount into total bill
+// And I click on SPLIT THE BILL
+// Then I expect to see that amount divvied up amongst the 4 patrons equally
+// Getting elements from the dom(HTML)
+let split = document.getElementsByClassName('split')[0]
+// add the event handler
+split.addEventListener('click', function() {
+  // grab teh value from total bill
+  let totalBill = document.getElementsByClassName('total_bill')[0].value
+  // split it 4 ways
+  let moneyOwed = totalBill / 4
+  // and add that value to each of the patrons input boxes
+  // take money owed and assign to each patron
+  // query the dom first to find each of patron classes
+  let patron1 = document.getElementById('patron_1')
+  patron1.value = moneyOwed
+})
+
+// Feature 2 - Credit Card Roulette
+// As a user
+// When I click on Credit Card Roulette
+// Then I expect to see an alert message "__Fill_in_the_blank person has lost and is paying!"
+// The person that pays is assigned randomly
 
 
-  // Feature 2 - Credit Card Roulette
-  // As a user
-  // When I click on Credit Card Roulette
-  // Then I expect to see an alert message "__Fill_in_the_blank person has lost and is paying!"
-  // The person that pays is assigned randomly
-
-
-  // Feature 3 - Find a menu price
-  // As a user
-  // When I enter a menu item
-  // Then I click on find menu item price
-  // I expect to see the price appear
+// Feature 3 - Find a menu price
+// As a user
+// When I enter a menu item
+// Then I click on find menu item price
+// I expect to see the price appear
 
